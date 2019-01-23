aa6345ecf8464d3fb178442bde6de338a9f7af46
贺钧威 <hejunwei@ziggurat.cn>
2019-01-23 10:23:33 +0800

    fix script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,11 +1,12 @@
 CHANNEL_NAME="mychannel"
 peer channel create -o orderer0.example-com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer0.example-com/msp/tlscacerts/tlsca.example-com-cert.pem
-
+sleep 5s
 peer channel join -b mychannel.block
-
+sleep 5s
 peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger/fabric/examples/chaincode/go/
-
+sleep 5s
 peer chaincode instantiate -o orderer0.example-com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer0.example-com/msp/tlscacerts/tlsca.example-com-cert.pem -C $CHANNEL_NAME -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}'
-
-
-peer chaincode invoke -o orderer.example-com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer.example-com/msp/tlscacerts/tlsca.example-com-cert.pem -C $CHANNEL_NAME -n mycc --peerAddresses peer0.org1-example-com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1-example-com/peers/peer0.org1-example-com/tls/ca.crt --peerAddresses peer0.org2.example-com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example-com/peers/peer0.org2.example-com/tls/ca.crt -c '{"Args":["invoke","a","b","10"]}'
+sleep 5s
+peer chaincode invoke -o orderer0.example-com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer0.example-com/msp/tlscacerts/tlsca.example-com-cert.pem -C $CHANNEL_NAME -n mycc --peerAddresses peer0.org1-example-com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1-example-com/peers/peer0.org1-example-com/tls/ca.crt -c '{"Args":["invoke","interfaceA"]}'
+sleep 5s
+peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["interfaceB"]}'
