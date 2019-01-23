cffa2b9e1e8503eefa516733ed9b930abcfd75df
贺钧威 <hejunwei@ziggurat.cn>
2019-01-23 11:55:42 +0800

    fix script

--- scripts/script.sh
+++ scripts/script.sh
@@ -7,6 +7,6 @@ peer chaincode install -n mycc -v 1.0 -p github.com/hyperledger/fabric/examples/
 sleep 5s
 peer chaincode instantiate -o orderer0.example-com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer0.example-com/msp/tlscacerts/tlsca.example-com-cert.pem -C $CHANNEL_NAME -n mycc -v 1.0 -c '{"Args":["init","a", "100", "b","200"]}'
 sleep 5s
-peer chaincode invoke -o orderer0.example-com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer0.example-com/msp/tlscacerts/tlsca.example-com-cert.pem -C $CHANNEL_NAME -n mycc --peerAddresses peer0.org1-example-com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1-example-com/peers/peer0.org1-example-com/tls/ca.crt -c '{"Args":["invoke","interfaceA"]}'
+peer chaincode invoke -o orderer0.example-com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example-com/orderers/orderer0.example-com/msp/tlscacerts/tlsca.example-com-cert.pem -C $CHANNEL_NAME -n mycc --peerAddresses peer0.org1-example-com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1-example-com/peers/peer0.org1-example-com/tls/ca.crt -c '{"Args":["interfaceA"]}'
 sleep 5s
 peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["interfaceB"]}'
